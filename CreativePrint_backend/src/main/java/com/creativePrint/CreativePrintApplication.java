b6387af63c0b00f6291a445d99ff73184d1be47f
mohammeddl <mohammed_daali2@um5.ac.ma>
2025-02-12 11:17:06 +0100

    Initialize CreativePrint backend with basic configuration and dependencies

---
+++ CreativePrint_backend/src/main/java/com/creativePrint/CreativePrintApplication.java
@@ -0,0 +1,13 @@
+package com.creativePrint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreativePrintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreativePrintApplication.class, args);
+	}
+
+}
---
+++ CreativePrint_backend/src/test/java/com/creativePrint/CreativePrintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creativePrint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreativePrintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
