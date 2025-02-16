6580e95b5ab0491fc0179d7e5f0c63119721988c
mohammeddl <mohammed_daali2@um5.ac.ma>
2025-02-16 20:33:11 +0100

    Add Redux store configuration and integrate lucide-react icons

--- creativePrint-frontend/src/App.tsx
+++ creativePrint-frontend/src/App.tsx
@@ -1,17 +1,26 @@
+import { Provider } from 'react-redux'
 import LoginForm from './components/auth/loginForm/LoginForm'
 import RegisterForm from './components/auth/registerForm/RegisterFrom'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import store from './store/store'
+import ProductsPage from './pages/products'
+import ProfileEditPage from './pages/profile/edit'
 
 function App() {
 
   return (
+    <Provider store={store}>
+
     <Router>
     <Routes>
       <Route path="/login" element={<LoginForm />} />
       <Route path="/register" element={<RegisterForm />} />
       <Route path="/" element={<LoginForm />} />
+      <Route path='/home' element={<ProductsPage />}/>
+      <Route path='/profile' element={<ProfileEditPage/> }/>
     </Routes>
   </Router>
+      </Provider>
   )
 }
 
